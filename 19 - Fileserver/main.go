44fa69fab80b3d2a0864f6c126be7f0a18b9111b
Ewerton Henrique Marschalk <ewerton.henrique@havan.com.br>
2023-05-11 15:01:57 -0300

    tratando argumentos

--- 19 - Fileserver/main.go
+++ 19 - Fileserver/main.go
@@ -4,12 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	path := "C:\\Users\\132160\\git\\Learning-Golang"
-	addr := ":80"
-	fs := http.FileServer(http.Dir(path))
-	fmt.Println("Server starting at 0.0.0.0", addr)
+
+	if len(os.Args) != 3 {
+		fmt.Println("Uso: go run main.go <diretorio> <porta>")
+		os.Exit(1)
+	}
+
+	httpDir := os.Args[1]
+	addr := ":" + os.Args[2]
+	fs := http.FileServer(http.Dir(httpDir))
+	fmt.Printf("Server starting at 0.0.0.0%s\n", addr)
 	log.Fatal(http.ListenAndServe(addr, fs))
 }
